ebe2f5fdcd8f5eaa082e9413858e2d9fd5aa1802
hongtinglin <hongting.lin0@gmail.com>
2011-08-05 23:15:02 +0800

    lht init commit

---
+++ Gemfile
@@ -0,0 +1,30 @@
+source 'http://rubygems.org'
+
+gem 'rails'
+
+# Bundle edge Rails instead:
+# gem 'rails',     :git => 'git://github.com/rails/rails.git'
+
+gem 'mysql2'
+
+group :development do
+  gem 'rspec-rails','2.6.1'
+end
+
+group :test do
+  gem 'rspec-rails','2.6.1'
+  gem 'webrat','0.7.1'
+end
+
+# Asset template engines
+
+
+# Use unicorn as the web server
+# gem 'unicorn'
+
+# Deploy with Capistrano
+# gem 'capistrano'
+
+# To use debugger
+# gem 'ruby-debug19', :require => 'ruby-debug'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '739bcbbbe05ba3bdc7f316978440b683258b0e60287605521bf340b2e4a1d8661e6583ee038848a4b06160aec50cbd42707c9e8e73ce0b9eebeed0ab9804cc1b'
