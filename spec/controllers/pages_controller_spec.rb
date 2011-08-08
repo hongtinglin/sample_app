31ae380d756b5265a1fb6a8f13e8299ae3a5d16d
hongtinglin <hongting.lin@ctquan.com>
2011-08-08 11:20:01 +0800

    done with static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,17 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title",:content=>"| home")
     end
   end
 
@@ -14,6 +20,20 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title",:content=>"| contact")
+    end
   end
 
+  describe "Get 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title",:content=>"| about")
+    end
+  end
 end
