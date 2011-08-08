a79cfb06471f28e38afe95a80f0984246a99d692
hongtinglin <hongting.lin0@gmail.com>
2011-08-09 00:04:32 +0800

    chapter 4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def title
+    base_title="ruby on rails tuturial sample app"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
