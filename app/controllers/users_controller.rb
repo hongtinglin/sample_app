4f810be4d083006d4a48b15898bd1fe81028d727
hongtinglin <hongting.lin@ctquan.com>
2011-08-12 19:31:27 +0800

    some chapter8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,15 @@ class UsersController < ApplicationController
     @title=@user.name
   end
 
+  def create
+    @user=User.new(params[:user])
+    if @user.save
+      redirect_to @user   
+    else
+      @title="sign up"
+      render 'new'
+    end
+  end
+
+
 end
